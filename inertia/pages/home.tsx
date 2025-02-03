8a983f824e50d3fad08bd2e0d83570783dc0af61
Alexis Bouchez <alexbcz@proton.me>
2025-02-03 14:58:28 +0100

    add shadcn ui

--- inertia/pages/home.tsx
+++ inertia/pages/home.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@/components/ui/button'
+
 export default function Home() {
   return (
     <>
       <h1 className="text-blue-600 underline">cl</h1>
+      <Button>Hello, World!</Button>
     </>
   )
 }
